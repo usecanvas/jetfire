813f0aa22b6778aa3a1803b94ab9f35218a649df
Sam Soffes <sam@soff.es>
2016-08-16 12:28:48 -0500

    Add Carthage support

---
+++ Jetfire.h
@@ -0,0 +1,18 @@
+//
+//  Jetfire.h
+//  Jetfire
+//
+//  Created by Sam Soffes on 8/16/16.
+//  Copyright © 2016 Cavas Labs, Inc. All rights reserved.
+//
+
+@import Foundation;
+
+//! Project version number for Jetfire.
+FOUNDATION_EXPORT double JetfireVersionNumber;
+
+//! Project version string for Jetfire.
+FOUNDATION_EXPORT const unsigned char JetfireVersionString[];
+
+#import <Jetfire/JFRSecurity.h>
+#import <Jetfire/JFRWebSocket.h>
